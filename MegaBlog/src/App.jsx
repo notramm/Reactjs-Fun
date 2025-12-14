d781902ee13561870d9194cb7b3c2f30283d9c5d
notramm <muduliram09@gmail.com>
2025-12-14 18:51:25 +0530

    MegaBlog project with Vite, React, and Appwrite integration

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import dotenv from 'dotenv'
+
+function App() {
+  console.log(import.meta.env.appWriteUrl);
+  
+
+  return (
+    <>
+      <h1>A Blog app with Appwrite </h1>
+    </>
+  )
+}
+
+export default App
