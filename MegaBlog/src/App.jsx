516350facc2a60b64bfbe4399a741db97fb6b8e4
notramm <muduliram09@gmail.com>
2025-12-15 18:09:28 +0530

    Implemented Redux for authentication with a new authSlice and store setup.

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,15 +1,41 @@
 import './App.css'
-import dotenv from 'dotenv'
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux';
+import authService from './appwrite/auth.js';
+import {login, logout} from './store/authSlice.js';
+import {Header, Footer} from './components';
+import { Outlet } from 'react-router-dom';
 
-function App() {
-  console.log(import.meta.env.appWriteUrl);
+function App() { 
   
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A Blog app with Appwrite </h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch( login(userData) );
+      }else{
+        dispatch( logout() );
+      }
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+  }), [];
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
