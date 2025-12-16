e3c581ad2cfd75300ec8d0e5dedbedd5cf83b6ba
notramm <muduliram09@gmail.com>
2025-12-16 18:25:08 +0530

    Add RTE component and PostForm for content creation; update styles and imports

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -2,6 +2,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo.jsx";
-import LogoutBtn from "./Header/LogoutBtn.jsx";;
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, RTE };
